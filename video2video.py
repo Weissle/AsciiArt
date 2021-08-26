6f7d61687007ca44a2520d2a1b0bf493f77f83a4
weissle <2016301500298@whu.edu.cn>
2021-08-27 01:57:57 +0800

    try better way to create video

--- video2video.py
+++ video2video.py
@@ -4,10 +4,10 @@ import utils
 import time 
 from img2img import bgrimg2charimg,bgrimg2charimg_color
 import multiprocessing
-from moviepy.editor import VideoFileClip
+from moviepy.editor import VideoFileClip,VideoClip
 import os
 from tqdm import trange
-
+import gc
 def multiprocess_warp(cvt_func,frame,*cfg_params):
 	cfg = utils.Config(*cfg_params)
 	return cvt_func(frame,cfg)
@@ -60,23 +60,45 @@ def video_convert(args):
 	if output_path == '':
 		output_path = utils.output_name_generator(input_path)
 
-	if args.no_audio:
-		create_slient_video(input_path,output_path,cvt_func)
-	else:
-		sli_vid_path = 'AsciiArt_TEMP_'+str(int(time.time()))+'.mp4'
-		create_slient_video(input_path,sli_vid_path,cvt_func)
+	ori_vid = VideoFileClip(input_path)
+	ori_duration = ori_vid.duration
+	w,h = ori_vid.size
 
-		ori_vid = VideoFileClip(input_path)
-		sli_vid = VideoFileClip(sli_vid_path)
-		audio = ori_vid.audio
-		dst_vid = sli_vid.set_audio(audio)
-		dst_vid.write_videofile(output_path)
+	pool = multiprocessing.Pool()
+	frames_output = []
 
-		sli_vid.close()
-		ori_vid.close()
-		dst_vid.close()
+	char_list = utils.get_char_list()
+	frames_output = []
+	for frame in ori_vid.iter_frames(logger='bar'):
+		h,w,_ = frame.shape
+		frames_output.append(pool.apply_async(multiprocess_warp,args=(cvt_func,frame,[h,w],args.scale,char_list)))
 
-		os.remove(sli_vid_path)
+	frames_count = len(frames_output)
+	cfg = utils.Config([h,w],args.scale,char_list)
+	def get_frame(t):
+		idx = int(frames_count/ori_duration*t)
+		if idx >= len(frames_output):
+			idx = -1
+		ret = np.array(frames_output[idx].get())
+		# special case for moviepy
+		if idx != 0:
+			frames_output[idx] = None
+		gc.collect()
+		return ret
+
+	dst_vid = VideoClip(get_frame,duration=ori_duration)
+
+	if not args.no_audio:
+		dst_vid.audio = ori_vid.audio
+
+	# dst_vid.write_videofile(output_path,fps=ori_vid.fps,threads=multiprocessing.cpu_count())	
+	dst_vid.write_videofile(output_path,fps=ori_vid.fps,threads=2)	
+
+	# pool.close()
+	# pool.join()
+	ori_vid.close()
+	dst_vid.close()
+
 if __name__ == '__main__':
 
 	start_time = time.time()
